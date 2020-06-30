e2bf182cca317da2df717ac7cb11405ce60e386b
Richard Cartwright <spark@streampunk.media>
2020-06-30 17:08:15 +0100

    Writing documentation and creating simple examples

---
+++ src/examples/ex2.ts
@@ -0,0 +1,14 @@
+import redio, { Funnel, end } from '../index'
+
+const wait = (t: number) => new Promise(r => setTimeout(() => r(t), t * 500))
+function addWait (n: number): Funnel<number> { 
+    let count = 0
+    return async () => (count < n) ? await wait(++count) : end 
+} 
+redio<number>(addWait(4)).doto(x => console.log('doto', x)).toArray().then(console.log)
+// output: doto 1  (after 0.5 s, then wait 1.0s)
+//         doto 2  (thwn wait 1.0s)
+//         doto 3  (then wait 1.5s)
+//         doto 4 
+//         [ 1, 2, 3, 4 ]
+
