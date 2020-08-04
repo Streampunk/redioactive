525f28c98ecd6e7874d0cfa775f6154a9bac6471
Richard Cartwright <spark@streampunk.media>
2020-08-04 16:47:19 +0100

    More sequence tests

--- src/__tests__/sequence.spec.ts
+++ src/__tests__/sequence.spec.ts
@@ -1,10 +1,9 @@
-import redio from '../redio'
+import redio, { end, Funnel, Liquid, RedioPipe, isPipe } from '../redio'
 
 describe('Testing sequences of pipes', () => {
-	test.only('Sequence should flatten pipes', async () => {
+	test('Sequence should flatten pipes', async () => {
 		await expect(
 			redio([redio([1, 2, 3]), redio([4, 5]), redio([6])])
-				.doto(console.log)
 				.sequence()
 				.toArray()
 		).resolves.toEqual([1, 2, 3, 4, 5, 6])
@@ -61,5 +60,37 @@ describe('Testing sequences of pipes', () => {
 				.toPromise()
 		).resolves.toEqual(6)
 		expect(eachFn).toHaveBeenCalledTimes(6)
+	})
+	function innerGenny(length: number, delay: number): Funnel<number> {
+		let lengthCount = 0
+		return () =>
+			new Promise<Liquid<number>>((resolve, _reject) => {
+				if (lengthCount++ >= length) {
+					resolve(end)
+				} else {
+					setTimeout(() => {
+						resolve(lengthCount)
+					}, delay)
+				}
+			})
+	}
+	function genny(streams: number, delay: number): Funnel<RedioPipe<number>> {
+		let streamCount = 0
+		return () =>
+			new Promise<Liquid<RedioPipe<number>>>((resolve, _reject) => {
+				if (streamCount++ >= streams) {
+					resolve(end)
+				} else {
+					setTimeout(() => {
+						resolve(redio(innerGenny(streamCount, 20)))
+					}, delay)
+				}
+			})
+	}
+	test.only('Slow producers', async () => {
+		await expect(redio(genny(3, 40)).sequence().toArray()).resolves.toEqual([1, 1, 2, 1, 2, 3])
+	})
+	test('Is it a pipe? Is it a plane?', async () => {
+		await expect(redio(genny(3, 20)).map(isPipe).toArray()).resolves.toEqual([true, true, true])
 	})
 })
